5b27986a9834b959d730751e939fd36b66025aee
ryanriccio05 <rriccio@villanova.edu>
2024-12-05 10:52:32 -0500

    added attempts at PDF Reading Files

--- simplesterms/src/index.js
+++ simplesterms/src/index.js
@@ -3,13 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import PDFUpload from './PDFUploader';
-import PDFReader from './PDFReader';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <PDFUpload />
+    <App />
   </React.StrictMode>
 );
 
